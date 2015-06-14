8fa4e26238129561f6c06d8eed1d9db8c6676290
Dolnor <timewalker75a@gmail.com>
2015-06-14 14:47:44 +0300

    package v1.1.3 + logging

--- Clover/Resources/clover-backup.sh
+++ Clover/Resources/clover-backup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+logfil="/Library/Logs/SCT/sct_install_`date +%Y%m%d`.log"
+echo "------------------ Clover Data Backup Start -----------------" >> $logfil
 
 ## determine date and timsetamp create backup folder for that day
 ds=`date '+%Y-%m-%d'`
 fbackup="$HOME/Desktop/Backups/$ds"
 if [ ! -e "${fbackup}" ]
 then
-    mkdir -p "${fbackup}"
+	mkdir -p "${fbackup}"
+	echo "$(date +%T): created $(echo $fbackup | cut -d/ -f4-)" >> $logfil
+else
+	echo "$(date +%T): using backup dir $(echo $fbackup | cut -d/ -f4-)" >> $logfil
 fi
 
 ##  determine efi partition
@@ -16,17 +21,25 @@ config="${efi}/EFI/CLOVER/config.plist"
 if [ -e $config ];
 then
     cp "${config}" "$HOME/Desktop/Backups/$ds/config.plist"
+	echo "$(date +%T): $config file backed up" >> $logfil
 fi
+
+# perhaps OEM folder used?
 
 ## backup ACPI tables just in case too
 if [ -e "${efi}/EFI/CLOVER/ACPI/patched" ]
 then
     cp -R "${efi}/EFI/CLOVER/ACPI/patched" "$HOME/Desktop/Backups/$ds/ACPI"
+	echo "$(date +%T): patched acpi tables backed up" >> $logfil
 fi
 
 ## delete previous CLOVER folder from ESP
 if [ -e "${efi}/EFI/CLOVER" ]
 then
     rm -R "${efi}/EFI/CLOVER"
+	echo "$(date +%T): previous clover folder removed from mapped efi partition" >> $logfil
 fi
+
+echo "------------------- Clover Data Backup End ------------------" >> $logfil
+
 
--- Common Files/Resources/backup.sh
+++ Common Files/Resources/backup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+logfil="/Library/Logs/SCT/sct_install_`date +%Y%m%d`.log"
+echo "-------------- Kernel Extension Backup Start --------------" >> $logfil
 
 ## backup and delete previously installed kexts from SLE
 ds=`date '+%Y-%m-%d'`
 backup="$HOME/Desktop/Backups/$ds/Extensions"
+
+echo "$(date +%T): backing up kernel extensions to $(echo $backup | cut -d/ -f4-)" >> $logfil
 while read list;
 do
     kext=`echo $list`
@@ -11,8 +15,12 @@ do
         if [ ! -e "${backup}" ]
         then
             mkdir -p "${backup}"
+			echo "$(date +%T): backup dir didn't exist, created one" >> $logfil
         fi
         cp -Rf "$2$kext" "${backup}"
         rm -rf "$2$kext"
+		echo "$(date +%T): $(basename $kext) backed up & removed" >> $logfil
     fi
 done < $1
+
+echo "---------------  Kernel Extension Backup End --------------" >> $logfil
